45f0688cac25cf66f98c2f1594c0bf7cb36c6f10
ookami.kb <ookami.kb@gmail.com>
2020-06-20 22:23:55 +0200

    :tv: Update config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,14 @@
 plugins {
     java
     kotlin("jvm") version "1.3.72"
+    `maven-publish`
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
-    testCompile("junit", "junit", "4.12")
+allprojects {
+    group = "com.github.ookami-kb"
+    version = "0.1.0"
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,15 +3,12 @@ plugins {
     kotlin("jvm")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testCompile("junit", "junit", "4.12")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+    testCompile("junit", "junit", "4.12")
 }
